8aa9ad05582d307ef9deef0b47ee6b6b7e27ed23
suhyun <86099781+Suhyun77@users.noreply.github.com>
2024-03-04 16:17:07 +0900

    feat : 1 target Train

--- Assets/Scripts/TrainingExample/MonkeyAgent.cs
+++ Assets/Scripts/TrainingExample/MonkeyAgent.cs
@@ -3,7 +3,6 @@ using Unity.MLAgents;
 using Unity.MLAgents.Sensors;
 using Unity.MLAgents.Actuators;
 
-// Code Monkey
 public class MonkeyAgent : Agent
 {
     [SerializeField] private Transform target;
